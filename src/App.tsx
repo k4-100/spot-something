52b18b2068a83c17c86487a2f8e2706b6b7a87b6
k4-100 <kmakingk@gmail.com>
2024-10-12 00:24:38 +0200

    implemented search bar and dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
+import { CssBaseline } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useEffect, useState } from "react";
+import Header from "./components/Header";
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
-  const [name, setName] = useState("sa")
-
-  useEffect(() => {
-  }, []);
 
   return (
-    <div className="container">
-      s
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <>
+        <Header />
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchAppBar from "./SearchAppbar";
+
+
+const Header: React.FC = () => {
+  return (
+    <>
+      <SearchAppBar />
+    </>
+  );
+
+}
+
+export default Header;
